22e84ab4b2c873870f8f28a209a7533afbb2edf3
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-11-28 13:08:06 -0500

    all methods has incomplete code

--- Lab3.3/src/inPlaceSorts.java
+++ Lab3.3/src/inPlaceSorts.java
@@ -2,11 +2,25 @@
 public class inPlaceSorts {
 	public static void main(String[] args)
 	{
-		int[] testList = {4, 5, 2, 7, 1, -6};
+		/*int[] testList = {4, 5, 2, 7, 1, -6};
 		insertionSort(testList);
 		for(int i = 0; i < testList.length; i++)
 		{
 			System.out.println(testList[i]);
+		}*/
+		
+		/*double[] testList2 = {4, 5, 2, 7, 1, -6};
+		selectionSort(testList2);
+		for(int i = 0; i < testList2.length; i++)
+		{
+			System.out.println(testList2[i]);
+		}*/
+		
+		String[] testList3 = {"best", "apple", "ever", "eaten"};
+		bubbleSort(testList3);
+		for(int i = 0; i < testList3.length; i++)
+		{
+			System.out.println(testList3[i]);
 		}
 		
 	}
@@ -42,15 +56,55 @@ public class inPlaceSorts {
 	
 	public static void selectionSort(double[] list1)
 	{
-		int swap = 0;
-		for(int i = 0; i < list1.length-1; i++)
+		int value = 0;
+		double min = 0;
+		int minPos = 0;
+		while(value != list1.length)
 		{
-			
+			for(int i = 0; i < list1.length; i++)
+			{
+				double swap;
+				for(int j = i + 1; j < list1.length; j++)
+				{
+					if(list1[i] > list1[j] && list1[j-1] > list1[j])
+					{
+						min = list1[j];
+						minPos = j;
+					}
+				}
+				if(list1[value] > min)
+				{
+					swap = list1[value];
+					list1[value] = min;
+					list1[minPos] = swap;
+					value++;
+				}	
+			}
 		}
 	}
 	
 	public static void bubbleSort(String[] list1)
 	{
-		
+		boolean swap = false;
+		while(!swap)
+		{
+			int numSwaps = 0;
+			for(int i = 0; i < list1.length-1; i++)
+			{
+				String value;
+				if(list1[i].compareTo("list1[i+1]") < 0)
+				{
+					value = list1[i+1];
+					list1[i+1] = list1[i];
+					list1[i] = value;
+					numSwaps++;
+				}
+					
+			}
+			if(numSwaps == 0)
+			{
+				swap = true;
+			}
+		}
 	}
 }
