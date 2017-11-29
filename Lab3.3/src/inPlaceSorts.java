e86445a93a412f5ce70e149cbea2cc8a9c2393e8
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-11-29 13:15:49 -0500

    l

--- Lab3.3/src/inPlaceSorts.java
+++ Lab3.3/src/inPlaceSorts.java
@@ -9,19 +9,19 @@ public class inPlaceSorts {
 			System.out.println(testList[i]);
 		}*/
 		
-		/*double[] testList2 = {4, 5, 2, 7, 1, -6};
+		double[] testList2 = {4, 5, 2, 7, 1, -6};
 		selectionSort(testList2);
 		for(int i = 0; i < testList2.length; i++)
 		{
 			System.out.println(testList2[i]);
-		}*/
+		}
 		
-		String[] testList3 = {"best", "apple", "ever", "eaten"};
+		/*String[] testList3 = {"best", "apple", "ever", "eaten", "achoo"};
 		bubbleSort(testList3);
 		for(int i = 0; i < testList3.length; i++)
 		{
 			System.out.println(testList3[i]);
-		}
+		}*/
 		
 	}
 	
@@ -57,16 +57,16 @@ public class inPlaceSorts {
 	public static void selectionSort(double[] list1)
 	{
 		int value = 0;
-		double min = 0;
-		int minPos = 0;
 		while(value != list1.length)
 		{
 			for(int i = 0; i < list1.length; i++)
 			{
 				double swap;
+				double min = list1[i];
+				int minPos = i;
 				for(int j = i + 1; j < list1.length; j++)
 				{
-					if(list1[i] > list1[j] && list1[j-1] > list1[j])
+					if(min > list1[j])
 					{
 						min = list1[j];
 						minPos = j;
@@ -92,19 +92,33 @@ public class inPlaceSorts {
 			for(int i = 0; i < list1.length-1; i++)
 			{
 				String value;
-				if(list1[i].compareTo("list1[i+1]") < 0)
+				if(list1[i].compareTo(list1[i+1]) > 0)
 				{
 					value = list1[i+1];
 					list1[i+1] = list1[i];
 					list1[i] = value;
 					numSwaps++;
-				}
-					
+				}		
 			}
-			if(numSwaps == 0)
+			if(numSwaps != 0)
+			{
+				for(int j = list1.length; j < 0; j--)
+				{
+					String value;
+					if(list1[j].compareTo(list1[j-1]) > 0)
+					{
+						value = list1[j+1];
+						list1[j+1] = list1[j];
+						list1[j] = value;
+						numSwaps++;
+					}		
+				}
+			}
+			else if(numSwaps == 0)
 			{
 				swap = true;
 			}
+			
 		}
 	}
 }
